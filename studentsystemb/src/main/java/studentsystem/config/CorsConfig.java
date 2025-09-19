417f8b1d3efc535729f70aa7a9e99aac8c63992a
Fidel Genre <trabajosfidel4@gmail.com>
2025-09-19 05:50:14 -0300

    aaaa

---
+++ studentsystemb/src/main/java/studentsystem/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package studentsystem.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(
+            @Value("${FRONTEND_URL}") String origin) {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(origin) // ej: https://studentsystemf.onrender.com
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
